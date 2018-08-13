30cb3b5df6c824a112a9492cd93749860e286d8e
Jeremías <jrmsdev@gmail.com>
2018-08-13 16:34:45 -0300

    nonblocking fileevent to read bg cmd out

--- lib/pkgtk/cmdexec.tcl
+++ lib/pkgtk/cmdexec.tcl
@@ -11,6 +11,7 @@ namespace eval ::cmdexec {
     variable pkg_rootdir ""
     variable query_format {%n %v (%sh)\n\n%e}
     variable list_format {%o|%n-%v}
+    variable bgdone 0
 
     if {[info exists ::env(PKGTK_ROOTDIR)]} {
         set pkg_rootdir $::env(PKGTK_ROOTDIR)
@@ -33,28 +34,46 @@ proc ::cmdexec::getcmd {args} {
 }
 
 #
+# read a line of output from running background command/process
+#
+proc ::cmdexec::bgread {out dryrun chan} {
+    if {[gets $chan line] >= 0} {
+        $out insert end "$line\n"
+        #~ update
+    }
+    if {[eof $chan]} {
+        puts "bgread: eof"
+        try {
+            puts "bgread: close"
+            close $chan
+        } trap CHILDSTATUS {results options} {
+            set rc [lindex [dict get $options -errorcode] 2]
+            set fatalerror [expr $dryrun && $rc == 1]
+            puts "bgread rc: $rc"
+            puts "bgread fatalerror: $fatalerror"
+            if {$fatalerror} {
+                utils show_error "ERROR: $cmd ($rc)\n$results"
+                $out insert end "*** ERROR: return code $rc\n"
+                $out insert end $results
+                #~ update
+            }
+        } finally {
+            puts "bgread: done!"
+            set cmdexec::bgdone 1
+        }
+    }
+}
+
+#
 # run command in background and insert lines of output
 #
 proc ::cmdexec::runbg {out args {dryrun 0}} {
+    set cmdexec::bgdone 0
     set cmd [join [cmdexec::getcmd $args] " "]
     set chan [open "|$cmd" "r"]
-    while {[gets $chan line] >= 0} {
-        $out insert end "$line\n"
-        update
-    }
-    try {
-        close $chan
-    } trap CHILDSTATUS {results options} {
-        set rc [lindex [dict get $options -errorcode] 2]
-        if {$dryrun && $rc == 1} {
-            return
-        } else {
-            utils show_error "ERROR: $cmd ($rc)\n$results"
-            $out insert end "*** ERROR: return code $rc\n"
-            $out insert end $results
-            update
-        }
-    }
+    fconfigure $chan -blocking 0 -buffering line
+    fileevent $chan readable [list cmdexec::bgread $out $dryrun $chan]
+    tkwait variable cmdexec::bgdone
 }
 
 #
